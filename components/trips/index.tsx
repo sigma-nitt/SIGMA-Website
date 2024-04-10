26aff90359fe010d5f41bbc7d06f79821e5058f4
unknown <rohitjalanirj@gmail.com>
2024-04-10 16:13:27 +0530

    trip slvd

--- components/trips/index.tsx
+++ components/trips/index.tsx
@@ -1,69 +1,3 @@
-// "use client"
-// import React, { useState } from 'react';
-// import './trips.css';
-
-// const images = [
-//   './images/john.jpg',
-//   './images/bkg.jpg',
-//   './images/bkg.jpg',
-//   './images/john.jpg',
-//   './images/bkg.jpg',
-//   './images/john.jpg',
-//   './images/john.jpg',
-//   './images/john.jpg',
-//   './images/john.jpg',
-//   './images/bkg.jpg',
-//   './images/john.jpg',
-//   './images/john.jpg',
-//   './images/bkg.jpg',
-//   './images/bkg.jpg',
-//   './images/john.jpg',
-//   './images/bkg.jpg',
-//   './images/john.jpg',
-//   './images/john.jpg',
-//   './images/john.jpg',
-//   './images/john.jpg',
-//   './images/bkg.jpg',
-//   './images/john.jpg',
-// ];
-
-// const Gallery: React.FC = () => {
-//   const [index, setIndex] = useState(0);
-//   const [transitionDuration, setTransitionDuration] = useState(3);
-
-//   const handleSwipeLeft = () => {
-//     setIndex(prevIndex => Math.min(prevIndex + 6, images.length - 6));
-//     setTransitionDuration(3);
-//   };
-
-//   const handleSwipeRight = () => {
-//     setIndex(prevIndex => Math.max(prevIndex - 6, 0));
-//     setTransitionDuration(3);
-//   };
-
-//   return (
-//     <div className="pr-40 pl-40 pt-20">
-//       <div style={{ display: 'flex', alignItems: 'center' }}>
-//         <button onClick={handleSwipeLeft} disabled={index + 6 >= images.length} style={{ fontSize: '3.5rem', padding: '0.5rem' }}>
-//           {"<"}
-//         </button>
-//         <div className="gallery-container" style={{ transition: `transform ${transitionDuration}s ease !important` }}>
-//           {images.slice(index, index + 6).map((image, idx) => (
-//             <div key={idx} className="image-wrapper">
-//               <img src={image} alt={`Image ${idx}`} className="image" />
-//             </div>
-//           ))}
-//         </div>
-//         <button onClick={handleSwipeRight} disabled={index === 0} style={{ fontSize: '3.5rem', padding: '0.5rem' }}>
-//           {">"}
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Gallery;
-
 "use client"
 import React, { useState } from 'react';
 import './trips.css';
@@ -113,9 +47,9 @@ const Gallery: React.FC = () => {
   };
 
   return (
-    <div className="pr-40 pl-40 pt-20">
+    <div className="pr-1 pl-1 pt-15 md:pr-40 md:pl-40 md:pt-20">
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <button onClick={handleSwipeLeft} disabled={index + 6 >= images.length} style={{ fontSize: '3.5rem', padding: '0.5rem' }}>
+        <button onClick={handleSwipeLeft} disabled={index + 6 >= images.length} style={{ fontSize: '1rem', padding: '0.5rem' }}>
           {"<"}
         </button>
         <div className="gallery-container" style={{ transition: `transform ${transitionDuration}s ease !important` }}>
@@ -125,7 +59,7 @@ const Gallery: React.FC = () => {
             </div>
           ))}
         </div>
-        <button onClick={handleSwipeRight} disabled={index === 0} style={{ fontSize: '3.5rem', padding: '0.5rem' }}>
+        <button onClick={handleSwipeRight} disabled={index === 0} style={{ fontSize: '1rem', padding: '0.5rem' }}>
           {">"}
         </button>
       </div>
