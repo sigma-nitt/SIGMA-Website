9fb98ef92dc74cbf3c3c8d03b8332c9ce8e3d48a
unknown <rohitjalanirj@gmail.com>
2024-04-19 00:19:35 +0530

    ch

--- components/Enigma/index.tsx
+++ components/Enigma/index.tsx
@@ -51,14 +51,14 @@ const PDFViewer: React.FC = () => {
 
   return (
     <div style={{ maxWidth: '5000px', margin: '0 auto' }}>
-      <div className="p-4 mb-8">
-        <h1 className="hdng bg-secondary-gradient-2 bg-clip-text text-transparent text-4xl text-center font-bold md:text-6xl">
-          TAKE A LOOK AT OUR MAGAZINE !
+      <div className="p-4 mb-8" style={{ textAlign: 'center' }}>
+        <h1 className="hdng bg-secondary-gradient-2 bg-clip-text text-transparent text-4xl font-bold md:text-6xl">
+          TAKE A LOOK AT OUR MAGAZINE!
         </h1>
       </div>
 
       {pdfDocuments.map((pdf, index) => (
-        <div className="box bg-white p-4" key={index} style={{ borderRadius: '8px', boxShadow: '0 4px 8px rgba(0,0,0,0.1)', width: '80%', margin: '0 auto', marginBottom: '80px'}}>
+        <div className="box bg-white p-4" key={index} style={{ borderRadius: '8px', boxShadow: '0 4px 8px rgba(0,0,0,0.1)', width: '80%', margin: '0 auto', marginBottom: '80px' }}>
           <div style={{ display: 'flex' }}>
             <div style={{ width: '70%', marginRight: '2%' }}>
               <iframe
@@ -69,18 +69,37 @@ const PDFViewer: React.FC = () => {
                 scrolling="auto"
               ></iframe>
             </div>
-            <div style={{ width: '30%', margin: 'auto', textAlign: 'center' }}>
-              <h2 className="pdf-title font-bold text-4xl text-blue-600 m-auto" style={{ wordWrap: 'break-word'}}>{pdf.title}</h2>
-              <p className="pdf-desc text-black text-sm" style={{ overflowWrap: 'break-word', wordWrap: 'break-word'}}>{pdf.description}</p>
-              <button className="enlarge-button bg-blue-500 text-white p-2 mt-5" style={{ borderRadius: '20px', cursor: 'pointer'}} onClick={() => openPdf(pdf.url)}>Enlarge View</button>
+            <div style={{ width: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', paddingLeft: '2%' }}>
+              <div>
+                <h2 className="pdf-title font-bold text-4xl text-blue-600 text-center mb-5 mt-5" style={{ wordWrap: 'break-word' }}>
+                  {pdf.title}
+                </h2>
+                <p className="pdf-desc text-black text-center text-sm" style={{ overflowWrap: 'break-word', wordWrap: 'break-word', margin: '0 auto' }}>
+                  {pdf.description}
+                </p>
+              </div>
+              <button
+                className="enlarge-button bg-blue-500 text-white p-2 w-2/5"
+                style={{ borderRadius: '20px', cursor: 'pointer', margin: '0 auto' }}
+                onClick={() => openPdf(pdf.url)}
+              >
+                Enlarge View
+              </button>
             </div>
           </div>
         </div>
       ))}
+
       {selectedPdf && (
         <div className="pdf-overlay">
           <div className="pdf-container">
-            <button className="bg-blue-500 text-white p-2" onClick={closePdf} style={{ borderRadius: '20px', position: 'absolute', top: '150px', right: '30px', zIndex: 10000 }}>Collapse</button>
+            <button
+              className="bg-blue-500 text-white p-2"
+              onClick={closePdf}
+              style={{ borderRadius: '20px', position: 'absolute', top: '150px', right: '30px', zIndex: 10000 }}
+            >
+              Collapse
+            </button>
             <iframe
               src={selectedPdf}
               style={{ width: '90%', height: 'calc(40% - 0px)', position: 'absolute', top: '150px', left: 20, zIndex: 9999 }}
