34c83f9260f6aaedc3126ba75fc732f2bc724932
unknown <rohitjalanirj@gmail.com>
2024-04-28 18:24:03 +0530

    test

--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -5,11 +5,11 @@ import { Testimonial } from "@/types/testimonial";
 export const testimonialData: Testimonial[] = [
   {
     id: 1,
-    name: "Anand Modi",
-    designation: "AbInBev",
+    name: " Sunil",
+    designation: "",
     image: image1,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "SIGMA provided me with an entry ticket to the world of business, finance and data. Honestly, I learned more from SIGMA, than from my management minor classes. And as a treasurer, I also did experience a 'lite' version of budgeting and financial planning that finance professionals experience in the real world! It's okay if you're into coding or engineering. If there's anything about SIGMA that hypes you up, I say apply!",
   },
   {
     id: 2,
