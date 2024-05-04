84164627fbc327fc89b0b31107d6a59361f67020
unknown <rohitjalanirj@gmail.com>
2024-05-04 18:42:20 +0530

    more test

--- components/Testimonial/testimonialData.ts
+++ components/Testimonial/testimonialData.ts
@@ -13,19 +13,19 @@ export const testimonialData: Testimonial[] = [
   },
   {
     id: 2,
-    name: "Anand Modi",
-    designation: "AbInBev",
+    name: "Thomas Mathew",
+    designation: "",
     image: image2,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Being the President of SIGMA during the tumultuous year of 2020-21 was nothing short of a transformative experience. Amidst the uncertainty of the pandemic, our club embraced change and emerged stronger than ever. From restructuring our organization to hosting engaging virtual events, SIGMA exemplified the unwavering determination of NITT students. Our dedication to excellence led to numerous collaborations with external partners, victories in case study competitions, and a remarkable 150% increase in recruitment. SIGMA isn't just a club; it's a platform for growth, leadership, and an unforgettable experience.",
   },
   {
     id: 3,
-    name: "Anand Modi",
+    name: "Vimalesan T",
     designation: "AbInBev",
     image: image1,
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit, ligula sit amet cursus tincidunt, lorem sem elementum nisi, convallis fringilla ante nibh non urna.",
+      "Sigma was the first club that accepted me after rejections from almost every team in NITT (partly because I was not a big talker initially later because of Rajesh Shaker), mainly due to Sundar, Nishant and Modi vouching for me during the selection process. It helped me to orient myself better for my career in management and has been a talking point in quite a few interviews leading to my MBA admission and even post that. One important learning I had was how far team building and respecting your peers can get you with respect to getting work done. Though I didn't really have the best farewell from the club, I still talk proudly on how some analytics tasks that comes up would be easily done by a Sigma junior when my colleagues and batchmates at IIM would complain about the difficulty and Sigma inadvertently led to the strengthening of my friendship with Sundar which I will always cherish. I still think I owe the juniors for my absence during the final year whatever the reasons may be, I felt less ownership over the club after that and it is something I have been working to avoid in other instances. Also, F 180DC and datatype I can't recollect the actual name of that club :p",
   },
   {
     id: 4,
