363c6b31ea215ab8b43ebf546f217b8ba3066d0a
unknown <rohitjalanirj@gmail.com>
2024-08-02 21:00:14 +0530

    tetx solve5

--- components/AboutUs/index.tsx
+++ components/AboutUs/index.tsx
@@ -161,7 +161,7 @@ const EventsPage: React.FC = () => {
           <h1 className="sigma text-4xl text-center mb-5 md:text-4xl pr-10">
             <span className="gradient-text">Experienced Leadership</span>
           </h1>
-          <p className="customParagraph">
+          <p>
             At SIGMA, we are proud to have a team of experienced leaders who are dedicated to providing our members with the best possible resources and opportunities. Our team consists of students from a variety of backgrounds who are passionate about business, data analytics, and machine learning. Our leaders have a wealth of experience in their respective fields, and they are committed to helping our members develop their skills and interests. They work hard to organize events, workshops, and other activities that provide our members with valuable learning experiences. We are always looking for new members to join our team, so if you are interested in becoming a part of SIGMA, please don't hesitate to get in touch.
           </p>
         </div>
