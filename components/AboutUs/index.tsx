faf988b857f5867853e6e63b0ab30b776096f867
unknown <rohitjalanirj@gmail.com>
2024-08-03 14:49:07 +0530

    mobile abtus solved

--- components/AboutUs/index.tsx
+++ components/AboutUs/index.tsx
@@ -99,18 +99,18 @@ const EventsPage: React.FC = () => {
     <div className="relative">
 
       {/* Our Story Section */}
-      <div className="flex flex-wrap pl-2 pr-2 mb-15 w-9/10 m-auto md:pr-0 md:pb-0 md:pl-20 mx-auto">
-        <div className="flex flex-col justify-center mt-50 w-full pr-8 md:w-1/2">
-          <h1 className="sigma text-4xl text-center md:text-4xl pr-10">
+      <div className="flex flex-wrap pl-2 pr-2 md:mb-15 w-10/10 md:w-9/10 m-auto md:pr-0 md:pb-0 md:pl-20 mx-auto">
+        <div className="flex flex-col justify-center mt-50 w-full sm:p-4 md:pr-8 md:w-1/2">
+          <h1 className="sigma text-center text-3xl pb-2 font-semibold lg:text-5xl md:text-4xl md:text-left md:pr-10 md:leading-none">
             <span className="gradient-text">We Power</span> the mindset of business
           </h1>
-          <p>
+          <p className="text-sm text-center lg:text-lg md:text-md md:text-left">
             SIGMA is a business club based in NIT Trichy, India. Our club was founded with the vision of providing learning resources for students from technical backgrounds who are interested in managerial and techno-managerial disciplines. Since then, we have grown to become a thriving community of students who are passionate about business, data analytics, and machine learning.
             At SIGMA, we offer a wide range of services and activities to our members, including publishing articles on Medium, social media posts about business topics, case studies, and projects involving data analytics and machine learning. We also organize workshops, podcasts, events, and competitions for college students in NIT Trichy.
             Our club is run by a team of experienced leaders who are committed to providing our members with the best possible resources and opportunities. We believe that by working together, we can achieve great things and make a positive impact in our community.
           </p>
         </div>
-        <div className="flex justify-center mt-50 mb-15 items-center w-full md:w-1/2 pr-8">
+        <div className="flex justify-center p-4 md:mt-50 md:mb-15 items-center w-full md:w-1/2 md:pr-8">
           <svg width="613" height="496" viewBox="0 0 1338 1313" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M753.247 734.786L694.697 642.78L752.47 579.233C753.777 577.76 754.45 575.832 754.345 573.866C754.239 571.9 753.362 570.055 751.905 568.731C750.448 567.407 748.528 566.711 746.561 566.793C744.593 566.875 742.738 567.729 741.396 569.17L686.363 629.697L648.493 570.19C647.818 569.127 646.885 568.252 645.782 567.645C644.678 567.039 643.439 566.72 642.18 566.72H597.286C595.944 566.719 594.627 567.079 593.473 567.762C592.318 568.445 591.368 569.426 590.723 570.602C590.077 571.778 589.76 573.106 589.804 574.447C589.848 575.788 590.251 577.092 590.973 578.223L649.522 670.219L591.749 733.814C591.074 734.538 590.55 735.39 590.207 736.318C589.864 737.247 589.708 738.235 589.75 739.224C589.791 740.213 590.028 741.184 590.448 742.081C590.867 742.978 591.46 743.782 592.193 744.448C592.926 745.114 593.784 745.628 594.716 745.96C595.649 746.292 596.639 746.435 597.627 746.382C598.616 746.329 599.585 746.08 600.476 745.65C601.368 745.22 602.166 744.617 602.823 743.876L657.856 683.35L695.726 742.857C696.407 743.911 697.342 744.777 698.445 745.375C699.548 745.974 700.784 746.285 702.039 746.28H746.933C748.273 746.28 749.589 745.919 750.742 745.237C751.895 744.554 752.844 743.574 753.489 742.4C754.134 741.225 754.452 739.899 754.41 738.56C754.367 737.221 753.965 735.917 753.247 734.786ZM706.145 731.317L610.913 581.683H638.037L733.306 731.317H706.145Z" fill="white"/>
             <path d="M670.555 938.888C829.089 938.888 957.634 812.474 957.634 656.5C957.634 500.526 829.089 374.112 670.555 374.112C512.021 374.112 383.476 500.526 383.476 656.5C383.476 812.474 512.021 938.888 670.555 938.888Z" stroke="#8A8FCF" stroke-width="2"/>
@@ -124,48 +124,44 @@ const EventsPage: React.FC = () => {
           </svg>
         </div>
       </div>
-      <div
-        className="bg-gradient"
-        style={{
-          background: 'linear-gradient(85.24deg, #1B1F2D -6.44%, #172C28 25.82%, #1E3432 57.01%, #111648 99.67%)',
-          height: '300px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          padding: '20px',
-        }}
-      >
-        <div className="flex flex-wrap items-center justify-between w-full max-w-7xl">
-          <div className="flex-1 mr-4">
-            <img 
-              src="./images/table.jpg" 
-              alt="Your Image Description" 
-              className="object-cover" 
+
+      <div className="bg-gradient-to-r from-[#1B1F2D] via-[#172C28] to-[#111648] flex items-center justify-center md:h-[300px] p-5">
+        <div className="flex flex-wrap items-center justify-between w-full max-w-7xl md:flex-row flex-col">
+          <div className="flex-1 mb-4 md:mb-0 md:mr-4">
+            <img
+              src="./images/table.jpg"
+              alt="Your Image Description"
+              className="object-cover w-full md:h-[250px]"
               style={{
-                height: '250px',
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
               }}
             />
           </div>
-          <div className="flex-1 ml-4 text-white">
-            <h2 className="text-2xl font-bold mb-4">Our Mission</h2>
-            <p>At SIGMA, we are committed to providing our members with a wide range of services and activities to help them develop their skills and interests in business, data analytics, and machine learning. Our mission is to empower our members with the knowledge and resources they need to succeed in their careers and make a positive impact in their communities.</p>
+          <div className="flex-1 ml-4 text-white md:ml-0">
+            <h2 className="text-center text-2xl font-bold mb-4">Our Mission</h2>
+            <p className="text-center">
+              At SIGMA, we are committed to providing our members with a wide range of services and activities to help them
+              develop their skills and interests in business, data analytics, and machine learning. Our mission is to empower
+              our members with the knowledge and resources they need to succeed in their careers and make a positive impact in
+              their communities.
+            </p>
           </div>
         </div>
       </div>
 
-      <div className="flex flex-wrap pl-2 pr-2 mb-15 w-9/10 m-auto md:pr-0 md:pb-0 md:pl-20 mx-auto">
-        <div className="flex flex-col justify-center w-full pr-8 md:w-1/2">
-          <h1 className="sigma text-4xl text-center mb-5 md:text-4xl pr-10">
+
+      <div className="flex flex-wrap pt-8 p-4 mb-15 md:w-9/10 m-auto md:pr-0 md:pb-0 md:pl-20 mx-auto">
+        <div className="flex flex-col justify-center w-full md:pr-8 md:w-1/2">
+          <h1 className="sigma text-4xl text-center mb-5 md:text-4xl md:pr-10">
             <span className="gradient-text">Experienced Leadership</span>
           </h1>
-          <p>
+          <p className="text-sm text-center lg:text-lg md:text-md md:text-left">
             At SIGMA, we are proud to have a team of experienced leaders who are dedicated to providing our members with the best possible resources and opportunities. Our team consists of students from a variety of backgrounds who are passionate about business, data analytics, and machine learning. Our leaders have a wealth of experience in their respective fields, and they are committed to helping our members develop their skills and interests. They work hard to organize events, workshops, and other activities that provide our members with valuable learning experiences. We are always looking for new members to join our team, so if you are interested in becoming a part of SIGMA, please don't hesitate to get in touch.
           </p>
         </div>
-        <div className="flex justify-center mt-20 mb-15 items-center w-full md:w-1/2 pr-8">
+        <div className="flex justify-center md:mt-20 md:mb-15 items-center w-full md:w-1/2 md:pr-8">
           <svg width="613" height="496" viewBox="0 0 1338 1313" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M753.247 734.786L694.697 642.78L752.47 579.233C753.777 577.76 754.45 575.832 754.345 573.866C754.239 571.9 753.362 570.055 751.905 568.731C750.448 567.407 748.528 566.711 746.561 566.793C744.593 566.875 742.738 567.729 741.396 569.17L686.363 629.697L648.493 570.19C647.818 569.127 646.885 568.252 645.782 567.645C644.678 567.039 643.439 566.72 642.18 566.72H597.286C595.944 566.719 594.627 567.079 593.473 567.762C592.318 568.445 591.368 569.426 590.723 570.602C590.077 571.778 589.76 573.106 589.804 574.447C589.848 575.788 590.251 577.092 590.973 578.223L649.522 670.219L591.749 733.814C591.074 734.538 590.55 735.39 590.207 736.318C589.864 737.247 589.708 738.235 589.75 739.224C589.791 740.213 590.028 741.184 590.448 742.081C590.867 742.978 591.46 743.782 592.193 744.448C592.926 745.114 593.784 745.628 594.716 745.96C595.649 746.292 596.639 746.435 597.627 746.382C598.616 746.329 599.585 746.08 600.476 745.65C601.368 745.22 602.166 744.617 602.823 743.876L657.856 683.35L695.726 742.857C696.407 743.911 697.342 744.777 698.445 745.375C699.548 745.974 700.784 746.285 702.039 746.28H746.933C748.273 746.28 749.589 745.919 750.742 745.237C751.895 744.554 752.844 743.574 753.489 742.4C754.134 741.225 754.452 739.899 754.41 738.56C754.367 737.221 753.965 735.917 753.247 734.786ZM706.145 731.317L610.913 581.683H638.037L733.306 731.317H706.145Z" fill="white"/>
             <path d="M670.555 938.888C829.089 938.888 957.634 812.474 957.634 656.5C957.634 500.526 829.089 374.112 670.555 374.112C512.021 374.112 383.476 500.526 383.476 656.5C383.476 812.474 512.021 938.888 670.555 938.888Z" stroke="#8A8FCF" stroke-width="2"/>
