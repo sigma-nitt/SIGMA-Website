44c1415c3dcc5bf08d2029737f71d87e612dc456
unknown <rohitjalanirj@gmail.com>
2024-08-13 10:35:29 +0530

    try1

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -221,7 +221,8 @@ const Header = () => {
         stickyMenu ? "bg-custom-gradient shadow transition duration-100 dark:bg-custom-gradient" : ""
       }`}
     >
-      <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
+      {/* <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0"> */}
+      <div className="relative mx-auto max-w-c-1390 items-center justify-between md:px-8 xl:flex 2xl:px-0">
         <div className="flex w-full items-center justify-between xl:w-1/4">
           <a href="/" className="ml-5">
             <Image
